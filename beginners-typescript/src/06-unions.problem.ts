3e93fd472610b1e168beacca36ad2dd309d7dc52
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-10-08 11:34:45 -0500

    Solution union types with 3 alternatives (direct, type and enum)

--- beginners-typescript/src/06-unions.problem.ts
+++ beginners-typescript/src/06-unions.problem.ts
@@ -1,3 +1,39 @@
+// Solution 1
+// interface User {
+//   id: number;
+//   firstName: string;
+//   lastName: string;
+//   /**
+//    * How do we ensure that role is only one of:
+//    * - 'admin'
+//    * - 'user'
+//    * - 'super-admin'
+//    */
+//   role: "admin" | "user" | "super-admin";
+// }
+
+// Solution 2
+// type SuperAdmin = "super-admin";
+// interface User {
+//   id: number;
+//   firstName: string;
+//   lastName: string;
+//   /**
+//    * How do we ensure that role is only one of:
+//    * - 'admin'
+//    * - 'user'
+//    * - 'super-admin'
+//    */
+//   role: "admin" | "user" | SuperAdmin
+// }
+
+
+// Solution 3
+enum RolesEnum {
+  Admin = "admin",
+  User = "user",
+  SuperAdmin = "super-admin"
+}
 interface User {
   id: number;
   firstName: string;
@@ -8,7 +44,7 @@ interface User {
    * - 'user'
    * - 'super-admin'
    */
-  role: string;
+  role: RolesEnum
 }
 
 export const defaultUser: User = {
