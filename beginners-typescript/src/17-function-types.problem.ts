7fa476d29b16fc14778d7c069f6a09902747f90b
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-10-10 23:44:12 -0500

    Listener 2 solutions

--- beginners-typescript/src/17-function-types.problem.ts
+++ beginners-typescript/src/17-function-types.problem.ts
@@ -3,16 +3,27 @@ import { Equal, Expect } from "./helpers/type-utils";
 /**
  * How do we type onFocusChange?
  */
-const addListener = (onFocusChange: (isFocused: boolean) => void) => {
+
+// Solucion 1
+// const addListener = (onFocusChange: (isFocused: boolean) => void) => {
+//   window.addEventListener("focus", () => {
+//     onFocusChange(true);
+//   });
+
+//   window.addEventListener("blur", () => {
+//     onFocusChange(false);
+//   });
+// };
+
+type FocusListener = (isFocused: boolean) => void;
+const addListener = (onFocusChange: FocusListener) => {
   window.addEventListener("focus", () => {
     onFocusChange(true);
   });
-
   window.addEventListener("blur", () => {
     onFocusChange(false);
   });
 };
-
 addListener((isFocused) => {
   console.log({ isFocused });
 
