d4d3793914be30882dc0de21772d4a8fb5aca80a
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-10-10 23:09:44 -0500

    Try Catch Manage

--- beginners-typescript/src/13-catch-blocks.problem.ts
+++ beginners-typescript/src/13-catch-blocks.problem.ts
@@ -1,12 +1,37 @@
 import { expect, it } from "vitest";
 
+// Solucion 1
+// const tryCatchDemo = (state: "fail" | "succeed") => {
+//   try {
+//     if (state === "fail") {
+//       throw new Error("Failure!");
+//     }
+//   } catch (e:any) {
+//     return e.message;
+//   }
+// };
+
+// Solucion 2
+// const tryCatchDemo = (state: "fail" | "succeed") => {
+//   try {
+//     if (state === "fail") {
+//       throw new Error("Failure!");
+//     }
+//   } catch (e) {
+//     return e as Error;
+//   }
+// };
+
+// Solucion 3
 const tryCatchDemo = (state: "fail" | "succeed") => {
   try {
     if (state === "fail") {
       throw new Error("Failure!");
     }
   } catch (e) {
-    return e.message;
+    if (e instanceof Error) {
+      return e.message;
+    }
   }
 };
 
