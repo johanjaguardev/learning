6d0c91703cdacdd35ea812c438adfa014af589e0
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2023-01-16 19:43:58 -0500

    squares and graphql

--- squares/src/App.tsx
+++ squares/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useId } from "react";
+import { useState, useId, useEffect } from "react";
 import "./App.scss";
 
 const App = () => {
@@ -11,15 +11,33 @@ const App = () => {
     false,
     false,
   ]);
+  const [fill, setFill] = useState(false);
+  const toggleSquare = (index: number) => {
+    const data = squares;
+    data[index] = !data[index];
 
+    setSquares(data);
+  };
+  useEffect(() => {
+    if (squares.indexOf(false) === -1) {
+      setFill(true);
+    }
+  });
   return (
     <div className="App">
-      <div className="squares__container">
-        {squares.map((sq) => (
-          <div className="square" key={useId()}>
-            {sq}
-          </div>
-        ))}
+      <div className={`squares ${fill ? "filled" : ""}`}>
+        {squares.map((sq, index) => {
+          console.log(sq);
+          return (
+            <button
+              className={`square ${sq ? "active" : ""}`}
+              key={index}
+              onClick={() => toggleSquare(index)}
+            >
+              {sq}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
