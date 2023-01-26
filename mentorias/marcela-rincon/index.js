9089a698ce9e90c200ab4b4f10ae06300ad4d341
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2023-01-26 18:47:10 -0500

    Co-authored-by: Amrincon28 <Amrincon28@users.noreply.github.com>

---
+++ mentorias/marcela-rincon/index.js
@@ -0,0 +1,5 @@
+var button = document.getElementById("boton");
+var parrafo = document.getElementsByTagName("p")[0];
+boton.addEventListener("click", function () {
+  parrafo.innerText = "hemos cambiado el texto en nuestra mentoria, ya ves";
+});
